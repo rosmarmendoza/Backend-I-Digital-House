eee30c738926a8a8094f26e8e0a4bf604ade5c21
rosmarmendoza <rosmar.mendoza@wearemo.com>
2022-11-15 09:15:10 -0500

    Commit for project

---
+++ DentalCare/src/main/java/com/dentalcare/DentalCare/DentalCareApplication.java
@@ -0,0 +1,14 @@
+package com.dentalcare.DentalCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalCareApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DentalCareApplication.class, args);
+	}
+
+}
